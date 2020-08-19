888fb06694eb1461e97f62f9e041ce1455122fd0
Tom Cunningham <tom.cunningham@aptible.com>
2020-08-19 09:48:58 -0400

    Home page logos

--- src/components/home/Logos.js
+++ src/components/home/Logos.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import cn from 'classnames';
 import healthifyLogo from '../../images/customers/logos/healthify.svg';
 import vergesenseLogo from '../../images/customers/logos/vergesense.svg';
 import mdsaveLogo from '../../images/customers/logos/mdsave.svg';
 import cityBlockLogo from '../../images/customers/logos/cityblock.svg';
-import pathaiLogo from '../../images/customers/logos/pathai.png';
+import simpleCitizenLogo from '../../images/customers/logos/simple-citizen.svg';
 import Container from '../container/Container';
 import styles from './Logos.module.css';
 
@@ -12,7 +13,7 @@ const Logos = () => (
     <h3>Companies that trust Aptible</h3>
 
     <div className={styles.logos}>
-      <div className={styles.logo}>
+      <div className={cn(styles.logo, styles.healthifyLogo)}>
         <img src={healthifyLogo} alt="Healthify logo" />
       </div>
       <div className={styles.logo}>
@@ -24,8 +25,8 @@ const Logos = () => (
       <div className={styles.logo}>
         <img src={cityBlockLogo} alt="Cityblock logo" />
       </div>
-      <div className={styles.logo}>
-        <img src={pathaiLogo} alt="PathAI logo" />
+      <div className={cn(styles.logo, styles.simpleCitizenLogo)}>
+        <img src={simpleCitizenLogo} alt="Simple Citizen logo" />
       </div>
     </div>
   </Container>
