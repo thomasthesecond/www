5eabf5d30b5d254c83d5a9ddf50151c5a2d3ef86
Tom Cunningham <tom.cunningham@aptible.com>
2020-08-26 10:51:49 -0400

    Print styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,7 @@ import './src/styles/typography.css';
 import './src/styles/forms.css';
 import './src/styles/search.css';
 import './src/styles/tables.css';
+import './src/styles/print.css';
 
 export function onRouteUpdate({ location, prevLocation }) {
   // Re-send Rollworks pixel on pushstate navigation changes
